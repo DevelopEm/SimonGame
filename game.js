5b51860197fa228fa539888dfa8eae9dc186d1df
DevelopEm <155112752+DevelopEm@users.noreply.github.com>
2024-02-02 16:47:28 +0100

    Update game.js

--- game.js
+++ game.js
@@ -37,11 +37,11 @@ function checkAnswer(currentLevel) {
     } else {
       playSound("wrong");
       $("body").addClass("game-over");
-      $("#level-title").text("Game Over☠️☠️☠️ </br> Tap on the screen to Restart")
+      $("#level-title").text("☠️☠️☠️Game Over☠️☠️☠️" "Tap on the screen to Restart")
 
       setTimeout(function() {
         $("body").removeClass("game-over");
-      }, 800);
+      }, 1200);
       $(startOver).click;
     }
 }
